9a5dd3d9f6e78f1c4ef4e293d09c244e8089cfa4
YogeshBarai <70328058+YogeshBarai@users.noreply.github.com>
2023-09-30 00:44:58 +0530

    refactor initial files. add logos and top nav bar

---
+++ src/components/TopNavBar.tsx
@@ -0,0 +1,29 @@
+import Logo from '../images/logos/PNG/DyneDollar_Logo_50x50px.png';
+import NameLogo from '../images/logos/PNG/DyneDollar_name_logo.png';
+
+
+const TopNavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="name-logo">
+        <img src={NameLogo} alt="DyneDollar" />
+      </div>
+      <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default TopNavBar;
